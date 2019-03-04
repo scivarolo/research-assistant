f7feab4066b5723b1981c5be95dd631b8b8ffa81
Sebastian Civarolo <scivarolo@gmail.com>
2019-03-04 15:41:21 -0600

    remove unused imports

--- research_assistant/views/auth_views.py
+++ research_assistant/views/auth_views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.template import RequestContext
-from django.urls import reverse
 from django.contrib import messages
 
 from research_assistant.forms import UserForm
--- research_assistant/views/index_views.py
+++ research_assistant/views/index_views.py
@@ -1,8 +1,4 @@
-from django.contrib.auth import logout, login, authenticate
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
-from django.template import RequestContext
 
 def index(request):
     template_name = "index.html"
