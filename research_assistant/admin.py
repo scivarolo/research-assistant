6ca3e4a2d51721129cf42a45a59a55ef179f8285
Sebastian Civarolo <scivarolo@gmail.com>
2019-03-05 10:53:06 -0600

    register models for admin view

--- research_assistant/admin.py
+++ research_assistant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Paper)
+admin.site.register(Tag)
+admin.site.register(List)
+admin.site.register(Author)
+admin.site.register(Journal)
