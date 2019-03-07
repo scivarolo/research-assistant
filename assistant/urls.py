c186189ce2f6321426bc96d715839cc50468ee18
Sebastian Civarolo <scivarolo@gmail.com>
2019-03-07 13:58:24 -0600

    add links to paper pdf and original source

--- assistant/urls.py
+++ assistant/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("select2/", include("django_select2.urls")),
-    path("", include("research_assistant.urls"))
-]
+    path("", include("research_assistant.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Makes media files available to the app
